71dbc21d5ec565c30420848f3baae1dfe011eedf
Sara Anspach <sara.anspach@gmail.com>
2018-09-07 22:56:31 -0400

    second

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,53 +1,54 @@
 
 var questions = [{
   trivia: "The Hound of the Baskervilles is a crime novel featuring which fictional detective?",
-  answers: ["Hercule Poirot", "Inspector Clouseau", "Sherlock Holmes", "Veronica Mars"],
+  answers: ["Hercule Poirot", "Inspector Clouseau", "Sherlock Holmes"],
   correctAnswer: "Sherlock Holmes",
 }, {
   trivia: "Which continent is also a country?",
-  answers: ["Brazil", "Africa", "USA", "Australia"],
+  answers: ["Brazil", "Africa", "Australia"],
   correctAnswer: "Australia",
 }, {
   trivia: "What was the name of the coffee shop in the hit sitcom friends?",
-  answers: ["Central Park", "Central Perks", "Starbucks", "Central Perk"],
+  answers: ["Central Park", "Central Perks", "Central Perk"],
   correctAnswer: "Central Perk",
 }, {
   trivia: "The Roman Catholic church La Sagrada Familia, located in Barcelona Spain, was designed by which Catalan architect?",
-  answers: ["Frank Lloyd Wright", "Eero Saarinen", "Antoni Gaudí", "Renzo Piano"],
+  answers: ["Eero Saarinen", "Antoni Gaudí", "Renzo Piano"],
   correctAnswer: "Antoni Gaudí",
 }, {
   trivia: "Who was the last queen of France prior to the French revolution?",
-  answers: ["Catherine de' Medici", "Marie Antoinette", "Marie Louise of Austria", "Margaret of Valois"],
+  answers: ["Catherine de' Medici", "Marie Antoinette", "Margaret of Valois"],
   correctAnswer: "Marie Antoinette",
 }, {
   trivia: "Come as You Are, a song by the grunge band Nirvana was released on which album?",
-  answers: ["Smells Like Teen Spirit", "Come as You Are", "The Man Who Sold the World", "Nevermind"],
+  answers: ["Smells Like Teen Spirit", "Come as You Are", "Nevermind"],
   correctAnswer: "Nevermind",
 }, {
   trivia: "According to legend, Romulus and Remus founded what city?",
-  answers: ["Paris", "Rome", "Prague", "Berlin"],
+  answers: ["Paris", "Rome", "Prague"],
   correctAnswer: "Rome",
 }, {
   trivia: "In the game of chess, how many pawns does each player start with?",
-  answers: ["12", "6", "8", "10"],
+  answers: ["6", "8", "10"],
   correctAnswer: "8",
 }, {
   trivia: "With twelve Oscar nominations and three wins, who is the most nominated male actor in Academy Awards history?",
-  answers: ["Jack Nicholson", "Leonardo DiCaprio", "Jeff Bridges", "Sean Penn"],
+  answers: ["Jack Nicholson", "Jeff Bridges", "Sean Penn"],
   correctAnswer: "Jack Nicholson",
 }, {
   trivia: "What is the tallest mountain in South America?",
-  answers: ["Aconcagua", "Ojos del Salado", "Huascarán", "Cotopaxi"],
+  answers: ["Aconcagua", "Ojos del Salado", "Huascarán"],
   correctAnswer: "Aconcagua",
 }, {
   trivia: "Dendrophobia is the fear of what?",
-  answers: ["Flying", "Trees", "Blushing", "Vomiting"],
+  answers: ["Flying", "Trees", "Blushing"],
   correctAnswer: "Trees",
 }, {
   trivia: "A Shakespearean sonnet consists of how many lines?",
-  answers: ["16", "15", "14", "17"],
+  answers: ["16", "15", "14"],
   correctAnswer: "14",
 }];
+
 
 
 var score = 0;
@@ -57,7 +58,7 @@ var questionIndex = 0;
 
 
   
-    function renderQuestion() {
+function renderQuestion() {
       
       if (questionIndex <= (questions.length - 1)) {
         document.querySelector("#question").innerHTML = questions[questionIndex].q;
@@ -78,6 +79,26 @@ var questionIndex = 0;
     renderQuestion();
     updateScore();
 
+    if (questionIndex === questions.length) {
+    }
+
+    if (userInput === "correctAnswer") {
+
+     
+      if (userInput === questions[questionIndex].a) {
+        alert("Correct!");
+        score++;
+        updateScore();
+      }
+      
+      else {
+        alert("Wrong!");
+      }
+
+      questionIndex++;
+      renderQuestion();
+
+    }
 
 
 
